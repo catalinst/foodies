4dd72543705f41d135e4aa691c28ab88424b4413
catalinst <catalinstoica1996@gmail.com>
2024-10-09 15:35:06 +0300

    add header and navigation, add basic pages

---
+++ app/meals/[meal]/page.js
@@ -0,0 +1,7 @@
+export default function MealDetailsPage() {
+  return (
+    <main>
+      meal
+    </main>
+  )
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,7 @@
+export default function ShareMealPage() {
+  return (
+    <main>
+      meals share
+    </main>
+  )
+}
