66f412bd64dde6e30700ef50f3b402f033384ae0
catalinst <catalinstoica1996@gmail.com>
2024-10-15 14:08:41 +0300

    upgrade meal details page, add custom not-found page and retrieve the meal from db

--- app/meals/[meal]/page.js
+++ app/meals/[meal]/page.js
@@ -1,7 +1,38 @@
-export default function MealDetailsPage() {
+import Image from 'next/image'
+import { notFound } from 'next/navigation'
+
+import { getMeal } from '@/lib/meals'
+import classes from './page.module.css'
+
+export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.meal)
+
+  if (!meal) {
+    notFound() // call the closest not-found page
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />')
+
   return (
-    <main>
-      meal
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        />
+      </main>
+    </>
   )
 }
